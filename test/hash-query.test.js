fa3135fd5f753eed888cc5cbdebc840a281d6c96
Alexandre Muniz <aom@puzzlezapper.com>
2019-03-13 15:42:16 -0700

    hash-query functions created test GREEN

--- test/hash-query.test.js
+++ test/hash-query.test.js
@@ -22,3 +22,55 @@ test('Write search to empty query', assert => {
     //assert
     assert.equal(result, expected);
 });
+
+test ('writing search to existing query changes search and resets page', assert => {
+    //arrange
+    const existingQuery = 'name=goblin&page=2';
+    const expected = 'name=dragon&page=1';
+    const searchOptions = {
+        name: 'dragon'
+    };
+    //act
+    const result = writeSearchToQuery(existingQuery, searchOptions);
+    //assert
+    assert.equal(result, expected);
+});
+
+function writePageToQuery(existingQuery, page) {
+    const searchParams = new URLSearchParams(existingQuery);
+    searchParams.set('page', page);
+    return searchParams.toString();
+}
+
+test ('write page to existing query', assert => {
+    //arrange
+    const existingQuery = 'name=goblin&page=1';
+    const page = 3;
+    const expected = 'name=goblin&page=3';
+
+    //act
+    const result = writePageToQuery(existingQuery, page);
+
+    //assert
+    assert.equal(result, expected);
+});
+
+function readFromQuery(query) {
+    const searchParams = new URLSearchParams(query);
+    const queryOptions = {
+        name: searchParams.get('name'),
+        page: parseInt(searchParams.get('page'))
+    };
+    return queryOptions;
+}
+
+test ('Read options from query', assert => {
+    const query = 'name=goblin&page=3';
+    const expected = {
+        name: 'goblin',
+        page: 3
+    };
+    //act
+    const result = readFromQuery(query);
+    assert.deepEqual(result, expected);
+});
