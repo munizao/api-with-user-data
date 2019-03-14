5b8398d4812abb082336db525c15afef620bfde8
Alexandre Muniz <aom@puzzlezapper.com>
2019-03-14 11:52:01 -0700

    Added loadHeader Function

--- src/header-component.js
+++ src/header-component.js
@@ -1,3 +1,5 @@
+import { auth } from './firebase.js';
+
 export function makeHeader() {
     const html = /*html*/ `
         <header>
@@ -21,4 +23,32 @@ export function makeProfile(user) {
     const template = document.createElement('template');
     template.innerHTML = html;
     return template.content;
+}
+
+const headerContainer = document.getElementById('header-container');
+
+export default function loadHeader(options) {
+    const dom = makeHeader();
+
+    const header = dom.querySelector('header');
+    headerContainer.appendChild(dom);
+
+    if(options && options.skipAuth) {
+        return;
+    }
+
+    auth.onAuthStateChanged(user => {
+        if(user) {
+            const userDom = makeProfile(user);
+            const signOutButton = userDom.querySelector('button');
+            signOutButton.addEventListener('click', () => {
+                auth.signOut();
+                window.location.hash = '';
+            });
+            header.appendChild(userDom);
+        }
+        else {
+            window.location = './auth.html' + window.location.hash;
+        }
+    });
 }
