2dfeb4e4dab55f44492e66361836497e6b0a3b79
Alexandre Muniz <aom@puzzlezapper.com>
2019-03-14 11:52:35 -0700

    added usersRef

--- src/firebase.js
+++ src/firebase.js
@@ -11,4 +11,5 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 
 const db = firebase.database();
+export const usersRef = db.ref('users');
 export const favoritesByUserRef = db.ref('favortes-by-user');
