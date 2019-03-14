b64a5bc32493c60fbf7ee949a918575b3da736ee
Alexandre Muniz <aom@puzzlezapper.com>
2019-03-14 10:56:43 -0700

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyApq5qo94jtFgtKDgoh4w8vKRH5RnelCV8",
+    authDomain: "auth-test-3342d.firebaseapp.com",
+    databaseURL: "https://auth-test-3342d.firebaseio.com",
+    projectId: "auth-test-3342d",
+    storageBucket: "auth-test-3342d.appspot.com",
+    messagingSenderId: "381284056290"
+};
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+
+const db = firebase.database();
+export const favoritesByUserRef = db.ref('favortes-by-user');
